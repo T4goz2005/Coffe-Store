d4ccc80a4cc76e658a679d6c017bfa91cc1a5854
T4goz2005 <thiagopedrero83@gmail.com>
2023-05-01 15:03:13 -0300

    Fist commit

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+function trocarImg (anything){
+    document.querySelector('.star-img').src = anything;
+}
+
+function changeCicleColor (color){
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
